589a5870d115a248ff42c566334cb99c5980fa0f
Hoxinying <he76430@gmail.com>
2020-11-17 07:24:35 +0000

    cheese

---
+++ L06/exercise/ttt_print_empty_chessboard.py
@@ -0,0 +1,17 @@
+cell_1 = 'x'
+cell_2 = 'x'
+cell_3 = 'O'
+cell_4 = '  '
+cell_5 = 'o'
+cell_6 = ' '
+cell_7 = ' x'
+cell_8 = ' '
+cell_9 = ' '
+
+x = (input("請輸入O或X"))
+
+print( cell_4,"|",cell_8,"|",cell_8)
+print("---+---+---")
+print(cell_4,"|",x,"|",cell_4)
+print("---+---+---")
+print( cell_4,"|",cell_8,"|",cell_4)
